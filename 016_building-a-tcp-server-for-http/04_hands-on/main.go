bc0c26b1ac122b638dde8b94c50fa2dd1024aad0
Paulo Santos <paulo_sss@hotmail.com>
2018-08-31 14:31:44 +0100

    building tcp server for http

---
+++ 016_building-a-tcp-server-for-http/04_hands-on/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err.Error())
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	// read request
+	request(conn)
+}
+
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			mux(conn, ln)
+		}
+		if ln == "" {
+			// headers are done
+			break
+		}
+		i++
+	}
+}
+
+func mux(conn net.Conn, ln string) {
+	// request line
+	m := strings.Fields(ln)[0]
+	u := strings.Fields(ln)[1]
+	fmt.Println("***METHOD", m)
+	fmt.Println("***URI", u)
+
+	// multiplexer
+	respond(conn, m, u)
+
+}
+
+func respond(conn net.Conn, m string, u string) {
+
+	var body string
+	if m == "GET" && u == "/" {
+		body = `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+		<strong>INDEX</strong><br>
+		<a href="/">index</a><br>
+		<a href="/about">about</a><br>
+		<a href="/contact">contact</a><br>
+		<a href="/apply">apply</a><br>
+		</body></html>`
+	}
+	if m == "GET" && u == "/about" {
+		body = `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+		<strong>ABOUT</strong><br>
+		<a href="/">index</a><br>
+		<a href="/about">about</a><br>
+		<a href="/contact">contact</a><br>
+		<a href="/apply">apply</a><br>
+		</body></html>`
+	}
+	if m == "GET" && u == "/contact" {
+		body = `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+		<strong>CONTACT</strong><br>
+		<a href="/">index</a><br>
+		<a href="/about">about</a><br>
+		<a href="/contact">contact</a><br>
+		<a href="/apply">apply</a><br>
+		</body></html>`
+	}
+	if m == "GET" && u == "/apply" {
+		body = `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+		<strong>APPLY</strong><br>
+		<a href="/">index</a><br>
+		<a href="/about">about</a><br>
+		<a href="/contact">contact</a><br>
+		<a href="/apply">apply</a><br>
+		<form method="POST" action="/apply">
+		<input type="submit" value="apply">
+		</form>
+		</body></html>`
+	}
+	if m == "POST" && u == "/apply" {
+		body = `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+		<strong>APPLY PROCESS</strong><br>
+		<a href="/">index</a><br>
+		<a href="/about">about</a><br>
+		<a href="/contact">contact</a><br>
+		<a href="/apply">apply</a><br>
+		</body></html>`
+	}
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
