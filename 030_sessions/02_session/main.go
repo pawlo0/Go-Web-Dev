ede0f9fecbeba1cde96cd6b2f75667e99b54a7be
Paulo Santos <paulo_sss@hotmail.com>
2018-09-28 18:13:59 +0100

    uuid and first session

--- 030_sessions/02_session/main.go
+++ 030_sessions/02_session/main.go
@@ -1,13 +1,83 @@
 package main
 
 import (
-	"github.com/satori/go.uuid"
 	"html/template"
+	"log"
 	"net/http"
+
+	uuid "github.com/satori/go.uuid"
 )
 
+type user struct {
+	UserName string
+	First    string
+	Last     string
+}
 
+var tpl *template.Template
+var dbUsers = map[string]user{}
+var dbSessions = map[string]string{}
 
-func main(){
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
 
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", bar)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	// get cookie
+	c, err := r.Cookie("session")
+	if err != nil {
+		sID := uuid.Must(uuid.NewV4())
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+
+	// if the user exists already get user
+	var u user
+	if un, ok := dbSessions[c.Value]; ok {
+		u = dbUsers[un]
+	}
+
+	// process for submission
+	if r.Method == http.MethodPost {
+		un := r.FormValue("username")
+		f := r.FormValue("firstname")
+		l := r.FormValue("lastname")
+		u = user{un, f, l}
+		dbSessions[c.Value] = un
+		dbUsers[un] = u
+	}
+
+	err = tpl.ExecuteTemplate(w, "index.gohtml", u)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Fatalln(err)
+	}
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+
+	//get cookie
+	c, err := r.Cookie("session")
+	if err != nil {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	un, ok := dbSessions[c.Value]
+	if !ok {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	u := dbUsers[un]
+	tpl.ExecuteTemplate(w, "bar.gohtml", u)
 }
